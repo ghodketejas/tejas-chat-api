ac0c7d3784a3756015788519f1694f45a3ed5e64
ghodketejas <ghodketg@mail.uc.edu>
2025-06-26 08:03:45 -0400

    enable CORS for chatbot API

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
+  // Enable CORS (Cross Origin Resource Sharing)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method === 'OPTIONS') {
     return res.status(200).send('OK');
   }
